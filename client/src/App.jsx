b42ff43f62d4b2983cc7adfa6951beb3e476ab8c
Avinash singh <118665810+iillimitable@users.noreply.github.com>
2023-10-19 22:06:31 +0530

    esate v3

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {FcSearch} from 'react-icons/fc'
+import {Link} from 'react-router-dom'
+
+export default function Header() {
+  return (
+    
+
+        <header className='bg-slate-200 shadow-md'>
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+
+              <Link to='/'>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                    <span className='text-slate-400'>Avinash</span>
+                    <span className='text-slate-700'>Estate</span>
+                </h1>
+                </Link>
+
+            <form className='bg-slate-100 rounded-lg p-3 flex items-center'>
+              <input type="text" placeholder='Search..' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+              <FcSearch/>
+            </form>
+            <ul className='flex gap-3'>
+              <Link to='/'>
+              <li className='hidden sm:inline hover:underlin '>Home</li>
+              </Link>
+
+              <Link to='/about'>
+              <li className='hover:underline'>About</li>
+              </Link>
+
+              <Link to='/sign-in'>
+              <li className='hover:underline'>Singin</li>
+              </Link>
+            </ul>
+            </div>
+        </header>
+      
+
+  )
+}
