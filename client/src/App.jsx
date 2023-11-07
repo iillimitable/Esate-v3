d9d44ea570091c067a73dd7e7f3c02d546042730
Avinash singh <118665810+iillimitable@users.noreply.github.com>
2023-11-07 19:59:23 +0530

    auth profile etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import Signin from './pages/Signin'
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import PrivateRouts from "./components/PrivateRouts";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-      <Route path='/sign-in' element={<Signin/>}/>
-      <Route path='/sign-up' element={<SignUp/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        <Route element={<PrivateRouts />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FcSearch } from "react-icons/fc";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,8 +33,16 @@ export default function Header() {
             <li className="hidden sm:inline hover:underline">About</li>
           </Link>
 
-          <Link to="/sign-in">
-            <li className="hover:underline">Sign in</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full w-9 h-9 object-cover"
+                src={currentUser.avatar}
+                alt={"profile"}
+              ></img>
+            ) : (
+              <li className="hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
